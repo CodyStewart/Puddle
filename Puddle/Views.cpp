3f092eb0ad4afc31f8fc71acc5917f9a68fd344e
CodyStewart <codystewart1789@gmail.com>
2024-03-13 21:57:11 -0400

    added the ability to more generically load game data from xml file

--- Puddle/Views.cpp
+++ Puddle/Views.cpp
@@ -6,36 +6,132 @@
 #include "poolBall.h"
 #include "poolTable.h"
 
-/* DebugView is used to check physics. It has:
-* Balls representing pool balls (or any balls)
-* Walls representing the walls of a pool table
-* Colliders for the balls and walls
-*/ 
 
-void loadStartViewResources(std::vector<Resource>& resources) {
-	Resource circleText("blackOutlineCircle.png");
-	resources.push_back(circleText);
+void loadStartView(View* view, ResourceManager* resManager, PuddleRenderer* renderer) {
+	//Resource circleText("blackOutlineCircle.png");
+	//resources.push_back(circleText);
 }
 
-void loadDebugViewResources(std::vector<Resource>& resources) {
+/* DebugView is used to check physics. It has:
+*	Balls representing pool balls (or any balls)
+*	Walls representing the walls of a pool table
+*	Colliders for the balls and walls
+*/ 
+
+void loadDebugView(View* view, ResourceManager* resManager, PuddleRenderer* renderer) {
 	tinyxml2::XMLDocument doc;
 	doc.LoadFile("assets/gameViewData.xml");
-	//doc.PrintError();
-	//printf("%d", doc.ErrorID());
-	const tinyxml2::XMLElement* viewElement = doc.FirstChildElement("View")->FirstChildElement("gameObjects")->FirstChildElement("oneBall");
-	const tinyxml2::XMLNode* node = viewElement->FirstChild();
-	const tinyxml2::XMLElement* element = node->ToElement();
-	const tinyxml2::XMLNode* node2 = element->FirstChild();
-	const tinyxml2::XMLText* text = node2->ToText();
-	const char* tex = text->Value();
+	doc.PrintError();
+	printf("%d", doc.ErrorID());
 
-	viewElement = viewElement->NextSiblingElement();
+	// find the correct element
+	const tinyxml2::XMLElement* debugElement = doc.FirstChildElement("DebugView");
+
+	// load the background texture
+	const char* bgTextureNode = debugElement->FirstChildElement()->FirstChild()->Value();
+
+	// set the game object element
+	const tinyxml2::XMLElement* gameObjectElement = debugElement->FirstChildElement("gameObjects");
+
+	// load the PoolBalls
+	const tinyxml2::XMLElement* poolBallElement = gameObjectElement->FirstChildElement("PoolBalls");
+	const tinyxml2::XMLNode* poolBall = poolBallElement->FirstChild();
+	for (int poolBallCount = 0; poolBallCount < poolBallElement->ChildElementCount(); poolBallCount++) {
+		// pull the data out of the xml file
+		
+		// get the resource file
+		const char* resourceText;
+		const tinyxml2::XMLNode* attribute = poolBall->FirstChild();
+		resourceText = attribute->FirstChild()->Value();
+		std::string resourceName(resourceText);
+		Resource objectResourceName(resourceName);
+
+		// get the position
+		SDL_Point position;
+		attribute = attribute->NextSibling();
+		const char* xVal = attribute->FirstChild()->FirstChild()->Value();
+		const char* yVal = attribute->FirstChild()->NextSibling()->FirstChild()->Value();
+		int xPos = std::stoi(xVal);
+		int yPos = std::stoi(yVal);
+		position.x = xPos;
+		position.y = yPos;
+
+		// get the radius
+		float radius;
+		attribute = attribute->NextSibling();
+		const char* radiusText = attribute->FirstChild()->Value();
+		radius = std::stof(radiusText);
+
+		// instantiate the object using the above data
+		ResHandleShrdPtr handle = resManager->getHandle(&objectResourceName);
+		if (handle.get()) {
+			Texture* texture = new Texture(renderer->getRenderer(), handle.get()->buffer(), handle.get()->size());
+			PoolBallPhysicsComponent* physicsComp = new PoolBallPhysicsComponent(position, radius);
+			GameObject* poolBallObject = new GameObject(new PoolBallInputComponent(), physicsComp, new PoolBallGraphicsComponent(renderer, physicsComp, texture));
+			view->addGameObject(poolBallObject);
+		}
+
+		// move on to the next poolBall object
+		poolBall = poolBall->NextSibling();
+	}
+
+	// load the PoolWalls
+	const tinyxml2::XMLElement* poolWallElement = gameObjectElement->FirstChildElement("PoolWalls");
+	const tinyxml2::XMLNode* poolWall = poolWallElement->FirstChild();
+	for (int poolWallCount = 0; poolWallCount < poolWallElement->ChildElementCount(); poolWallCount++) {
+		// get the resource file
+		const char* resourceText;
+		const tinyxml2::XMLNode* attribute = poolWall->FirstChild();
+		resourceText = attribute->FirstChild()->Value();
+		std::string resourceName(resourceText);
+		Resource objectResourceName(resourceName);
+
+		// get the position
+		SDL_Point position;
+		attribute = attribute->NextSibling();
+		const char* xVal = attribute->FirstChild()->FirstChild()->Value();
+		const char* yVal = attribute->FirstChild()->NextSibling()->FirstChild()->Value();
+		int xPos = std::stoi(xVal);
+		int yPos = std::stoi(yVal);
+		position.x = xPos;
+		position.y = yPos;
+
+		// get the width and height
+		int width, height = 0;
+		attribute = attribute->NextSibling();
+		const char* widthText = attribute->FirstChild()->Value();
+		attribute = attribute->NextSibling();
+		const char* heightText = attribute->FirstChild()->Value();
+		width = std::stoi(widthText);
+		height = std::stoi(heightText);
+
+		ResHandleShrdPtr handle = resManager->getHandle(&objectResourceName);
+		if (handle.get()) {
+			Texture* texture = new Texture(renderer->getRenderer(), handle.get()->buffer(), handle.get()->size());
+			PoolWallPhysicsComponent* physicsComp = new PoolWallPhysicsComponent(position, width, height);
+			GameObject* poolWallObject = new GameObject(new PoolWallInputComponent(), physicsComp, new PoolWallGraphicsComponent(renderer, physicsComp, texture));
+			view->addGameObject(poolWallObject);
+		}
+
+		poolWall = poolWall->NextSibling();
+	}
+
+	//const tinyxml2::XMLNode* node = poolBallNode->FirstChild();
+	//const tinyxml2::XMLNode* node2 = node->FirstChild();
+	//const char* tex = node2->Value();
+
+	// load the PoolWalls
+
+	//viewElement = viewElement->NextSiblingElement();
+
+	// sort the game objects by their z-values so that we render objects layered correctly
+
 	
 
-	Resource circleText("blackOutlineCircle.png");
-	Resource RectTexture("blackOutlineRect.png");
-	resources.push_back(circleText);
-	resources.push_back(RectTexture);
+	//Resource circleText("blackOutlineCircle.png");
+	//Resource RectTexture("blackOutlineRect.png");
+	//resources.push_back(circleText);
+	//resources.push_back(RectTexture);
 }
 
 bool loadView(std::vector<Resource>& resources, View* view, ResourceManager* resManager, PuddleRenderer* renderer) {
@@ -89,13 +185,13 @@ View::View(ViewState viewState, ResourceManager* resManager, PuddleRenderer* ren
 	switch (viewState) {
 	case startView:
 		// get the startView resources
-		loadStartViewResources(resources);
+		loadStartView(this, resManager, renderer);
 		// load the resources and instantiate the game objects
-		loadView(resources, this, resManager, renderer);
+		//loadView(resources, this, resManager, renderer);
 		break;
 	case debugView:
-		loadDebugViewResources(resources);
-		loadView(resources, this, resManager, renderer);
+		loadDebugView(this, resManager, renderer);
+		//loadView(resources, this, resManager, renderer);
 		break;
 	default:
 		printf("In loadView: viewState is not valid!\n");
